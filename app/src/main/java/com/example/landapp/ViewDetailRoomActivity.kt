dbabdc0f85558ea4c7196d9e4f38ef52aed9161d
ichbong94 <ichbong94@gmail.com>
2022-06-01 09:54:39 +0900

    방표현 XML 추가

---
+++ app/src/main/java/com/example/landapp/ViewDetailRoomActivity.kt
@@ -0,0 +1,11 @@
+package com.example.landapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewDetailRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.view_detail_room)
+    }
+}
