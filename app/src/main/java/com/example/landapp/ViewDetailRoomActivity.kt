8a5436eab4bc634076a34b64ce08cd649c0bd5f9
ichbong94 <ichbong94@gmail.com>
2022-06-01 11:08:13 +0900

    방목록 클릭릭시 상세화면 이동

--- app/src/main/java/com/example/landapp/ViewDetailRoomActivity.kt
+++ app/src/main/java/com/example/landapp/ViewDetailRoomActivity.kt
@@ -2,10 +2,21 @@ package com.example.landapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.landapp.datas.Room
+import kotlinx.android.synthetic.main.view_detail_room.*
 
 class ViewDetailRoomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_detail_room)
+
+        val roomData = intent.getSerializableExtra("room") as Room
+
+        priceTxt.text = roomData.price.toString()
+        descriptionTxt.text = roomData.description
+        addressTxt.text = roomData.address
+
+
+
     }
 }
